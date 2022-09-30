9ee9f9cb83a862f1097e7698a90ead98190167e6
inexperiencedCoder32 <112755149+inexperiencedCoder32@users.noreply.github.com>
2022-09-30 15:27:37 +0400

    Add files via upload

---
+++ Lesson AND homework 3/Homework3.py
@@ -0,0 +1,8 @@
+name = raw_input("what is your name: ")
+surname = raw_input("Surname here: ")
+age = raw_input("enter your age: ")
+height = raw_input("your height here: ")
+grade = raw_input("enter your grade: ")
+hobby = raw_input("what's your hobby: ")
+
+print("My name is {} {} i am {} years old {} centimeters in height, i am in the {}th grade,my hobby is {}.".format(name,surname,age,height,grade,hobby))
---
+++ Lesson AND homework 3/lesson3.py
@@ -0,0 +1,11 @@
+Name = "Oto"
+Surname = "Adamashvili"
+Age = 16
+Hobbies = "Programming"
+Study = "Goal-Oriented Academy"
+
+print("My name is {} {}, I am {} years old. I love {}, I study at {}." .format(Name,Surname,Age,Hobbies,Study))
+
+
+MyName = input("Oto")
+output(MyName)
